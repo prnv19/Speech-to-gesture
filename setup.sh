7791cfc226c9741ef23f231f4251f88e9f149522
Pranav <78401509+prnv19@users.noreply.github.com>
2023-06-10 11:31:17 +0530

    initial

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Download and install Miniconda (a lightweight version of Anaconda)
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda
+rm ~/miniconda.sh
+
+# Add conda to the system PATH
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# Activate the conda base environment
+conda init
+source ~/.bashrc
+
+conda create --name stg-nv python=3.6 -y
+conda activate stg-nv
+pip install --user nvidia-pyindex
+export PATH=$PATH:$HOME/.local/bin
+conda install -c conda-forge openmpi -y
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/anaconda3/envs/stg-nv/lib/
+pip install --user nvidia-tensorflow[horovod]
